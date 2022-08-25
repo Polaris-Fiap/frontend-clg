13a80fddd835d2c9f4012fd854e6a257291fbf91
YDan08 <dasyamashita@gmail.com>
2022-08-25 15:49:21 -0300

    feat: adicionar tela de home

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { View } from "react-native"
+
+export const Home = () => {
+  return <View style={{ flex: 1, backgroundColor: "#0099ff" }}></View>
+}
+
+export default Home
