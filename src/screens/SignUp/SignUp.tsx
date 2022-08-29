3389068da1b414f66071142940e49ccc25ee68ca
YDan08 <dasyamashita@gmail.com>
2022-08-29 17:21:09 -0300

    feat: criar pagina de cadastro

---
+++ src/screens/SignUp/SignUp.tsx
@@ -0,0 +1,60 @@
+import { useNavigation } from "@react-navigation/native"
+import React from "react"
+import { SafeAreaView, View, StyleSheet, StatusBar } from "react-native"
+import { Button, Text, TextInput } from "react-native-paper"
+import { theme } from "../../styles/theme"
+
+export const SignUp = () => {
+  const navigation = useNavigation()
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.grupoInput}>
+        <Text variant='headlineMedium'>Sign Up</Text>
+        <TextInput label='Email' mode='flat' style={styles.input} placeholder='Digite seu nome completo' />
+        <TextInput label='Email' mode='flat' style={styles.input} placeholder='Digite seu email' />
+        <View style={styles.inputPassword}>
+          <TextInput label='Senha' mode='flat' style={styles.input} placeholder='Digite sua senha' />
+        </View>
+
+        <Button mode='contained' style={styles.botao}>
+          Cadastrar
+        </Button>
+        <Button onPress={() => navigation.navigate("login")}>Já tem uma conta? Entre aqui</Button>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default SignUp
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+  },
+  grupoInput: {
+    justifyContent: "center",
+    marginHorizontal: 40,
+  },
+  input: {
+    marginTop: 20,
+    backgroundColor: theme.primary,
+  },
+  text: {
+    fontSize: 42,
+  },
+  botao: {
+    backgroundColor: theme.primary,
+    marginVertical: 20,
+  },
+  texto: {
+    marginBottom: 18,
+  },
+  inputPassword: {
+    marginBottom: 20,
+  },
+  buttonEsqueceuSenha: {
+    width: 160,
+  },
+})
