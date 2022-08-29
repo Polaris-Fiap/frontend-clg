2e9be85b309cadc93acd93f563db6a12f1244266
YDan08 <dasyamashita@gmail.com>
2022-08-29 17:20:34 -0300

    feat: adicionar statusbar

--- src/screens/SearchSpecialists/SearchSpecialists.tsx
+++ src/screens/SearchSpecialists/SearchSpecialists.tsx
@@ -1,8 +1,18 @@
 import React from "react"
-import { View } from "react-native"
+import { StatusBar, StyleSheet, View } from "react-native"
+import { theme } from "../../styles/theme"
 
 export const SearchSpecialists = () => {
-  return <View style={{ flex: 1, backgroundColor: "#f0554a" }}></View>
+  return <View style={styles.container}></View>
 }
 
 export default SearchSpecialists
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    backgroundColor: "#f0554a",
+  },
+})
