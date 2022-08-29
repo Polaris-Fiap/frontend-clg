6db68b45a08d5b764a039f08631a3840ccc62ad5
YDan08 <dasyamashita@gmail.com>
2022-08-29 17:20:21 -0300

    feat: adicionar logout e statusbar

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,16 +1,28 @@
 import { useNavigation } from "@react-navigation/native"
-import React from "react"
-import { View, Button } from "react-native"
+import React, { useContext } from "react"
+import { View, Button, StyleSheet, StatusBar } from "react-native"
+import { AuthContext } from "../../contexts"
+import { theme } from "../../styles/theme"
 import { routes } from "../../utils/constants"
 
 export const Home = () => {
   const navigation = useNavigation()
-
+  const { handleLogout } = useContext(AuthContext)
   return (
-    <View style={{ flex: 1, backgroundColor: "#0099ff" }}>
+    <View style={styles.container}>
       <Button title='Buscar especialistas' onPress={() => navigation.navigate("buscaEspecialistas")} />
+      <Button title='logout' onPress={() => handleLogout()} />
     </View>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    backgroundColor: theme.primary,
+  },
+})
