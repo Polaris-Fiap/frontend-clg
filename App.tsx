2a68266e150a1cba67e7f2dedbea99731889f738
YDan08 <dasyamashita@gmail.com>
2022-08-29 21:06:21 -0300

    refactor: mover AuthProvider

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
-import AuthProvider from "./src/contexts"
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import AuthProvider from './src/contexts'
 
-import { Routes } from "./src/routes"
-import Login from "./src/screens/Login"
+import { Routes } from './src/routes'
+import Login from './src/screens/Login'
 
 export default function App() {
   return (
-    <AuthProvider>
-      <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
-        <StatusBar />
-        <Routes />
-      </SafeAreaView>
-    </AuthProvider>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
+      <StatusBar />
+      <Routes />
+    </SafeAreaView>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
-import { NavigationContainer } from "@react-navigation/native"
-import { useContext } from "react"
-import { AuthContext } from "../contexts"
+import { NavigationContainer } from '@react-navigation/native'
+import { useContext } from 'react'
+import AuthProvider, { AuthContext } from '../contexts'
 
-import { LoggedoutRoutes } from "./drawer.routes"
-import { LoginRoutes } from "./tab.routes"
+import { LoggedoutRoutes } from './drawer.routes'
+import { LoginRoutes } from './tab.routes'
 
 export const Routes = () => {
   const { logged } = useContext(AuthContext)
   return (
-    <NavigationContainer>{logged === true ? <LoginRoutes /> : <LoggedoutRoutes />}</NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>{logged === true ? <LoginRoutes /> : <LoggedoutRoutes />}</NavigationContainer>
+    </AuthProvider>
   )
 }
