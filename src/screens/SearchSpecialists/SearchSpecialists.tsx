17f79899fee55c61d56269c6e5d210a5227ccd99
YDan08 <dasyamashita@gmail.com>
2022-08-31 17:43:46 -0300

    feat: adicionar inputSearch

--- src/screens/SearchSpecialists/SearchSpecialists.tsx
+++ src/screens/SearchSpecialists/SearchSpecialists.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
 
 import ContainerView from '../../components/ContainerView'
+import InputSearch from '../../components/InputSearch'
 
 export const SearchSpecialists = () => {
-  return <ContainerView></ContainerView>
+  const [search, setSearch] = useState<string>('')
+  return (
+    <ContainerView>
+      <View>
+        <Text variant="titleLarge" style={styles.titulo}>
+          Encontre um especialista aqui
+        </Text>
+        <InputSearch value={search} onChangeText={setSearch} />
+      </View>
+    </ContainerView>
+  )
 }
+
+const styles = StyleSheet.create({
+  titulo: {
+    marginBottom: 12,
+    textAlign: 'center'
+  }
+})
 
 export default SearchSpecialists
