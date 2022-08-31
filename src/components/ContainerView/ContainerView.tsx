3f7ef7cf3a3e6fe78dd0ef0041e84d5aa759e173
YDan08 <dasyamashita@gmail.com>
2022-08-31 17:42:49 -0300

    refactor: trocar cor

--- src/components/ContainerView/ContainerView.tsx
+++ src/components/ContainerView/ContainerView.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components/native'
 export const ContainerView = styled.View`
   flex: 1;
   justify-content: center;
-  background-color: #fffae3;
+  background-color: #ffffff;
 `
 export default ContainerView
