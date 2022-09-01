5d8d300c24481af9d1721f8a62191f3fafc73107
YDan08 <dasyamashita@gmail.com>
2022-09-01 19:41:55 -0300

    feat: adicionar estilização e testar a navegação com perfil de especialista

---
+++ src/screens/PerfilEspecialista/PerfilEspecialista.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+export const PerfilEspecialista = () => {
+  return (
+    <SafeAreaView>
+      <Text>Perfil</Text>
+    </SafeAreaView>
+  )
+}
+
+export default PerfilEspecialista
--- src/screens/SearchSpecialists/SearchSpecialists.tsx
+++ src/screens/SearchSpecialists/SearchSpecialists.tsx
@@ -1,19 +1,23 @@
+import { useNavigation } from '@react-navigation/native'
 import React, { useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Button } from 'react-native'
 import { Text } from 'react-native-paper'
-
 import ContainerView from '../../components/ContainerView'
 import InputSearch from '../../components/InputSearch'
+import { theme } from '../../styles/theme'
 
 export const SearchSpecialists = () => {
   const [search, setSearch] = useState<string>('')
+  const navigation = useNavigation()
   return (
     <ContainerView>
-      <View>
+      <View style={styles.container}>
         <Text variant="titleLarge" style={styles.titulo}>
           Encontre um especialista aqui
         </Text>
-        <InputSearch value={search} onChangeText={setSearch} />
+        <InputSearch value={search} onChangeText={setSearch} style={styles.search} />
+
+        <Button title="Buscar especialistas" onPress={() => navigation.navigate('perfilEspecialista')} />
       </View>
     </ContainerView>
   )
@@ -22,7 +26,18 @@ export const SearchSpecialists = () => {
 const styles = StyleSheet.create({
   titulo: {
     marginBottom: 12,
-    textAlign: 'center'
+    textAlign: 'center',
+    color: theme.lightBlue
+  },
+  search: {
+    width: 320,
+    backgroundColor: theme.cultured
+  },
+  container: {
+    alignItems: 'center'
+  },
+  image: {
+    width: 60
   }
 })
 
