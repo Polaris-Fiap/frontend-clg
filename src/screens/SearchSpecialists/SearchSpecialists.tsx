7446abeadd52e49c6e2a6f730d9d210e8d8f2264
YDan08 <dasyamashita@gmail.com>
2022-09-02 13:06:53 -0300

    feat: criar pagina de busca por especialistas

--- src/screens/SearchSpecialists/SearchSpecialists.tsx
+++ src/screens/SearchSpecialists/SearchSpecialists.tsx
@@ -1,43 +1,64 @@
-import { useNavigation } from '@react-navigation/native'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react'
-import { StyleSheet, View, Button } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import { Text } from 'react-native-paper'
-import ContainerView from '../../components/ContainerView'
+import CardEspecialista from '../../components/CardEspecialista'
 import InputSearch from '../../components/InputSearch'
 import { theme } from '../../styles/theme'
+import daniel from '../../assets/daniel.jpg'
+import claudio from '../../assets/caludio.jpg'
+import guilherme from '../../assets/guilherme.jpg'
+import henrique from '../../assets/henrique.jpg'
+
+const especialistas = [
+  { nome: 'Daniel', especialidade: 'Cardiologia', img: daniel },
+  { nome: 'Claudio', especialidade: 'Neurologista', img: claudio },
+  { nome: 'Guilherme', especialidade: 'Cardiologia', img: guilherme },
+  { nome: 'Henrique', especialidade: 'Neurologista', img: henrique }
+]
 
 export const SearchSpecialists = () => {
   const [search, setSearch] = useState<string>('')
-  const navigation = useNavigation()
-  return (
-    <ContainerView>
-      <View style={styles.container}>
-        <Text variant="titleLarge" style={styles.titulo}>
-          Encontre um especialista aqui
-        </Text>
-        <InputSearch value={search} onChangeText={setSearch} style={styles.search} />
 
-        <Button title="Buscar especialistas" onPress={() => navigation.navigate('perfilEspecialista')} />
-      </View>
-    </ContainerView>
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.content}>
+          <Text variant="titleLarge" style={styles.titulo}>
+            Encontre um especialista aqui
+          </Text>
+          <InputSearch value={search} onChangeText={setSearch} style={styles.search} />
+          {especialistas.map(especialista => (
+            <CardEspecialista
+              nome={especialista.nome}
+              especialidade={especialista.especialidade}
+              img={especialista.img}
+              key={especialista.nome}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.cultured
+  },
   titulo: {
-    marginBottom: 12,
     textAlign: 'center',
     color: theme.lightBlue
   },
   search: {
     width: 320,
-    backgroundColor: theme.cultured
+    backgroundColor: theme.cultured,
+    marginVertical: 24
   },
-  container: {
-    alignItems: 'center'
-  },
-  image: {
-    width: 60
+  content: {
+    alignItems: 'center',
+    marginTop: 20
   }
 })
 
