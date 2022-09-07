93bed1b7a8d6e916b5c1622f340e1bdce1094f31
YDan08 <dasyamashita@gmail.com>
2022-09-07 18:45:16 -0300

    feat: fazer sistema de login

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,42 +1,97 @@
 import { useNavigation } from '@react-navigation/native'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { TextInput, Button, Text } from 'react-native-paper'
 import ContainerView from '../../components/ContainerView'
 import { AuthContext } from '../../contexts'
 import { theme } from '../../styles/theme'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useForm, Controller } from 'react-hook-form'
+import { Paciente } from '../SignUp'
+import Toast from 'react-native-toast-message'
+
+interface Login {
+  email: string
+  senha: string
+}
 
 export const Login = () => {
   const { handleLogin } = useContext(AuthContext)
+  const { handleSubmit, control } = useForm<Login>()
+  const [user, setUser] = useState<Paciente>()
   const navigation = useNavigation()
+
+  const onLogin = (dados: Login) => {
+    if (dados.email === user?.email && dados.senha === user.senha) {
+      handleLogin()
+    } else {
+      Toast.show({
+        type: 'error',
+        text1: 'Email ou senha inválidos.'
+      })
+    }
+  }
+
+  const getUser = async () => {
+    try {
+      const json = await AsyncStorage.getItem('@user')
+      const user = json && JSON.parse(json)
+      setUser(user)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
   return (
     <ContainerView>
       <View style={styles.login}>
         <Text variant="headlineMedium" style={styles.texto}>
           Login
         </Text>
-        <TextInput
-          label="Email"
-          mode="outlined"
-          style={styles.input}
-          placeholder="Digite seu email"
-          activeOutlineColor={theme.lightBlue}
+        <Controller
+          name="email"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              label="Email"
+              mode="outlined"
+              style={styles.input}
+              placeholder="Digite seu email"
+              activeOutlineColor={theme.lightBlue}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
+
         <View style={styles.viewPassword}>
-          <TextInput
-            label="Senha"
-            mode="outlined"
-            style={styles.input}
-            placeholder="Digite sua senha"
-            activeOutlineColor={theme.lightBlue}
+          <Controller
+            name="senha"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                label="Senha"
+                mode="outlined"
+                style={styles.input}
+                placeholder="Digite sua senha"
+                activeOutlineColor={theme.lightBlue}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
+
           <View style={styles.viewButtonEsqueceuSenha}>
             <Button style={styles.buttonEsqueceuSenha} textColor={theme.lightBlue}>
               Esqueceu a senha?
             </Button>
           </View>
         </View>
-        <Button mode="contained" style={styles.botao} onPress={() => handleLogin()}>
+        <Button mode="contained" style={styles.botao} onPress={handleSubmit(onLogin)}>
           Entrar
         </Button>
         <Button onPress={() => navigation.navigate('cadastro')} textColor={theme.lightBlue}>
