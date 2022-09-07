7e3aab7864164d7f8378bec9a3cbcfcf8bc8e331
YDan08 <dasyamashita@gmail.com>
2022-09-07 19:30:38 -0300

    feat: criar sistema de login

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,28 +1,57 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 import { ReactFCProps } from '../@types/geral'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Login, Paciente } from '../screens'
+import Toast from 'react-native-toast-message'
 
 interface AuthContextProps {
   logged?: boolean
-  handleLogin: () => void
+  user?: Paciente
+  handleLogin: (dados: Login) => void
   handleLogout: () => void
 }
 
 export const AuthContext = createContext<AuthContextProps>({
-  handleLogin: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleLogin: (dados: Login) => {},
   handleLogout: () => {}
 })
 
 export const AuthProvider: React.FC<ReactFCProps> = ({ children }: ReactFCProps) => {
   const [logged, setLogged] = useState<boolean>(false)
-  const handleLogin = () => {
-    setLogged(true)
+  const [user, setUser] = useState<Paciente>()
+  const handleLogin = (dados: Login) => {
+    if (dados.email === user?.email && dados.senha === user.senha) {
+      setLogged(true)
+    } else {
+      Toast.show({
+        type: 'error',
+        text1: 'Email ou senha inválidos.'
+      })
+    }
   }
   const handleLogout = () => {
     setLogged(false)
   }
+  const getUser = async () => {
+    try {
+      const json = await AsyncStorage.getItem('@user')
+      const user = json && JSON.parse(json)
+      setUser(user)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ handleLogin, handleLogout, logged }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ handleLogin, handleLogout, logged, user }}>
+      {children}
+    </AuthContext.Provider>
   )
 }
 
