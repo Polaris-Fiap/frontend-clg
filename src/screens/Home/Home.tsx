28302f8b1f130b0309749f8009ffee336759a1e2
YDan08 <dasyamashita@gmail.com>
2022-09-07 19:31:11 -0300

    feat: estilizar home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,15 +1,40 @@
 import React, { useContext } from 'react'
-import { Button } from 'react-native'
-import ContainerView from '../../components/ContainerView'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
+
 import { AuthContext } from '../../contexts'
+import { theme } from '../../styles/theme'
 
 export const Home = () => {
-  const { handleLogout } = useContext(AuthContext)
+  const { user } = useContext(AuthContext)
+
   return (
-    <ContainerView>
-      <Button title="logout" onPress={() => handleLogout()} />
-    </ContainerView>
+    <ScrollView style={styles.container}>
+      <View style={styles.viewBemVindo}>
+        <Text style={styles.texto} variant="headlineMedium">
+          Olá {user?.nome}
+        </Text>
+        <Text style={styles.texto} variant="bodyLarge">
+          AcallMe está feliz por ter você aqui!
+        </Text>
+      </View>
+    </ScrollView>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f7f7f7',
+    padding: 40
+  },
+  viewBemVindo: {
+    justifyContent: 'center',
+    marginTop: 30
+  },
+  texto: {
+    color: theme.lightBlue
+  }
+})
