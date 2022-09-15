8a43738d2e30cee8cbf82600aef0c2e4681e4303
YDan08 <dasyamashita@gmail.com>
2022-09-15 05:02:24 -0300

    feat: alterar rota das imagens

--- src/utils/constants/especialistas.ts
+++ src/utils/constants/especialistas.ts
@@ -1,7 +1,7 @@
-import daniel from '../../assets/daniel.jpg'
-import claudio from '../../assets/caludio.jpg'
-import guilherme from '../../assets/guilherme.jpg'
-import henrique from '../../assets/henrique.jpg'
+import daniel from '../../assets/imgs/daniel.jpg'
+import claudio from '../../assets/imgs/claudio.jpg'
+import guilherme from '../../assets/imgs/guilherme.jpg'
+import henrique from '../../assets/imgs/henrique.jpg'
 
 export const especialistas = [
   { nome: 'Daniel', especialidade: 'Cardiologia', img: daniel },
