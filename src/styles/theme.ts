7e1e405ea545a3fbf85f4b62b95476ccdde9d45f
YDan08 <dasyamashita@gmail.com>
2022-09-15 05:01:37 -0300

    feat: adicionar fontes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,17 @@
 export const theme = {
-  lightBlue: '#61c8f8',
-  darkGreen: '#071e22',
-  cornsilk: '#FFFAE3',
-  burnishedBrow: '#A37774',
-  deepTaupe: '#7D6167',
-  white: '#ffffff',
-  cultured: '#F7F7F7'
+  colors: {
+    lightBlue: '#61c8f8',
+    darkGreen: '#071e22',
+    cornsilk: '#FFFAE3',
+    burnishedBrow: '#A37774',
+    deepTaupe: '#7D6167',
+    white: '#ffffff',
+    cultured: '#F7F7F7'
+  },
+  text: {
+    texto: 'Roboto_400Regular',
+    titulo: 'Roboto_700Bold'
+  }
 }
 
 export type Theme = typeof theme
