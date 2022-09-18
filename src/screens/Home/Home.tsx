7a6170cd0350c7bd068356aa7dec86326e2a3076
YDan08 <dasyamashita@gmail.com>
2022-09-18 13:20:56 -0300

    feat: remover teste de branch

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -5,7 +5,6 @@ import { Avatar, Text } from 'react-native-paper'
 import { AuthContext } from '../../contexts'
 import { theme } from '../../styles/theme'
 import { useNavigation } from '@react-navigation/native'
-import Texto from '../../components/Texto'
 
 export const Home = () => {
   const { user } = useContext(AuthContext)
@@ -29,7 +28,6 @@ export const Home = () => {
           />
         )}
       </View>
-      <Texto info="aula com o prof daniel" />
     </ScrollView>
   )
 }
