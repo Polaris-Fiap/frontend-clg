2a24118372c50e65bac402e9efece97fc28a7fa9
YDan08 <dasyamashita@gmail.com>
2022-10-15 14:45:01 -0300

    feat: dividir dados

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -7,27 +7,48 @@ import { theme } from '../../styles/theme'
 import { useNavigation } from '@react-navigation/native'
 
 export const Home = () => {
-  const { user } = useContext(AuthContext)
+  const { user, especialista } = useContext(AuthContext)
   const navigation = useNavigation()
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.viewBemVindo}>
-        <View>
-          <Text style={styles.texto} variant="titleLarge">
-            Olá
-          </Text>
-          <Text style={styles.textoNome} variant="headlineMedium">
-            {user?.nomePaciente && user.nomePaciente.split(' ')[0]}
-          </Text>
+      {user && (
+        <View style={styles.viewBemVindo}>
+          <View>
+            <Text style={styles.texto} variant="titleLarge">
+              Olá
+            </Text>
+            <Text style={styles.textoNome} variant="headlineMedium">
+              {user?.nomePaciente && user.nomePaciente.split(' ')[0]}
+            </Text>
+          </View>
+          {user?.nomePaciente && (
+            <Avatar.Text
+              size={52}
+              label={user?.nomePaciente.split(' ')[0].substring(0, 1)}
+              onTouchStart={() => navigation.navigate('perfilUser')}
+            />
+          )}
         </View>
-        {user?.nomePaciente && (
-          <Avatar.Text
-            size={52}
-            label={user?.nomePaciente.split(' ')[0].substring(0, 1)}
-            onTouchStart={() => navigation.navigate('perfilUser')}
-          />
-        )}
-      </View>
+      )}
+      {especialista && (
+        <View style={styles.viewBemVindo}>
+          <View>
+            <Text style={styles.texto} variant="titleLarge">
+              Olá
+            </Text>
+            <Text style={styles.textoNome} variant="headlineMedium">
+              {especialista?.nomeEspecialista && especialista.nomeEspecialista.split(' ')[0]}
+            </Text>
+          </View>
+          {especialista?.nomeEspecialista && (
+            <Avatar.Text
+              size={52}
+              label={especialista?.nomeEspecialista.split(' ')[0].substring(0, 1)}
+              onTouchStart={() => navigation.navigate('perfilUser')}
+            />
+          )}
+        </View>
+      )}
     </ScrollView>
   )
 }
