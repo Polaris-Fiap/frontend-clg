55969e0197a448ffa170fe4fe5e58b3097c77abe
YDan08 <dasyamashita@gmail.com>
2022-10-15 12:49:16 -0300

    feat: consumir api de busca especialista

--- src/screens/SearchSpecialists/SearchSpecialists.tsx
+++ src/screens/SearchSpecialists/SearchSpecialists.tsx
@@ -1,26 +1,78 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ScrollView, StyleSheet, View } from 'react-native'
 import { Text } from 'react-native-paper'
+import { api } from '../../api'
 import CardEspecialista from '../../components/CardEspecialista'
 import InputSearch from '../../components/InputSearch'
 import { theme } from '../../styles/theme'
-import { especialistas } from '../../utils/constants/especialistas'
+
+export interface Especialista {
+  codEspecialista: number
+  nomeEspecialista: string
+  tipo: 'PJ' | 'PF'
+  dataNascimento: string
+  email: string
+  tipoEspecialidade: string
+  descricaoSobre: string
+  valorConsulta: string
+  genero: {
+    nomeGenero: string
+  }
+  endereco: {
+    cep: string
+    complemento: string
+    nomeRua: string
+    numeroRua: string
+    bairro: {
+      nomeBairro: string
+      cidade: {
+        nomeCidade: string
+        siglaCidade: string
+        estado: {
+          nomeEstado: string
+          siglaEstado: string
+        }
+      }
+    }
+  }
+  telefone?: string
+  senha: string
+  cpf?: string
+  cpfDigito?: string
+  cnpj?: string
+  cnpjDigito?: string
+  razaoSocial?: string
+}
 
 export const SearchSpecialists = () => {
   const [search, setSearch] = useState<string>('')
-  const [listaEspecialistas, setListaEspecialistas] = useState<typeof especialistas>(especialistas)
 
-  const changeListaEspecilista = (valor: string) => {
-    setSearch(valor)
-    if (valor !== '') {
-      setListaEspecialistas(
-        especialistas.filter(especialista => especialista.nome.toLowerCase().includes(valor.toLowerCase()))
-      )
-    } else {
-      setListaEspecialistas(especialistas)
+  const [especialistas, setEspecialistas] = useState<Especialista[]>()
+
+  // const changeListaEspecilista = (valor: string) => {
+  //   setSearch(valor)
+  //   if (valor !== '') {
+  //     setListaEspecialistas(
+  //       especialistas.filter(especialista => especialista.nome.toLowerCase().includes(valor.toLowerCase()))
+  //     )
+  //   } else {
+  //     setListaEspecialistas(especialistas)
+  //   }
+  // }
+
+  const buscarEspecialista = async () => {
+    try {
+      const pf = await api.get<Especialista[]>('/api/especialistaPf')
+      const pj = await api.get<Especialista[]>('/api/especialistaPj')
+      setEspecialistas([...pf.data, ...pj.data])
+    } catch (e) {
+      console.log(e)
     }
   }
+  useEffect(() => {
+    buscarEspecialista()
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -29,15 +81,20 @@ export const SearchSpecialists = () => {
           <Text variant="titleLarge" style={styles.titulo}>
             Encontre um especialista aqui
           </Text>
-          <InputSearch value={search} onChangeText={changeListaEspecilista} style={styles.search} />
-          {listaEspecialistas.map(especialista => (
-            <CardEspecialista
-              nome={especialista.nome}
-              especialidade={especialista.especialidade}
-              img={especialista.img}
-              key={especialista.nome}
-            />
-          ))}
+          <InputSearch value={search} onChangeText={setSearch} style={styles.search} />
+          {especialistas ? (
+            especialistas.map((especialista, key) => (
+              <CardEspecialista
+                nome={especialista.nomeEspecialista}
+                tipo={especialista.tipo}
+                id={especialista.codEspecialista}
+                especialidade={especialista.tipoEspecialidade}
+                key={`${especialista.nomeEspecialista}-${key}`}
+              />
+            ))
+          ) : (
+            <Text>Não existe especialistas no momento</Text>
+          )}
         </View>
       </ScrollView>
     </View>
